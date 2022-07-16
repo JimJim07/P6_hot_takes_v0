e71d3a8bff65c7bf6850ecea2a68439fdf629da2
Jimmy Clairy <jimcl@hotmail.fr>
2022-07-16 18:25:24 +0200

    update P6

--- password.js
+++
@@ -1,5 +0,0 @@
-// MongoDB
-exports.mongoDB = "mongodb+srv://p6_hot_takes:kfWuN69fXrlNEQ6L@cluster0.sdyji.mongodb.net/p6_hot_takes?retryWrites=true&w=majority";
-
-// Token
-exports.token   = "yWwMbK6ZP7w52ih479aG9je5kje8jUN34Qp5f83TF8QKDAW4cJu35L5TRbd9d48uT3D4u7wqmNqBHPUt5eMsa843syWNLCF72b9TWRj2p4y4u5Y";

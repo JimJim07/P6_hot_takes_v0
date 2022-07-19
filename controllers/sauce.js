b33b64d1953423ae7e0162a21213ae9855710532
Jimmy Clairy <jimcl@hotmail.fr>
2022-07-19 16:40:46 +0200

    sup limit

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -83,7 +83,7 @@ exports.getOneSauce = (req, res) => {
 
 // GET ALL SAUCES
 exports.getAllSauces = (req, res) => {
-    Sauce.find().limit()
+    Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
 };
